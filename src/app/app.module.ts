c15f7eb5eda56d3666ec402bf58b9943b25f9ef3
Benjamin Martinez <mail@stumx.info>
2022-07-05 17:13:58 -0500

    Migrated to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { PageLandingComponent } from './page-landing/page-landing.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    PageLandingComponent,
+    PresentationComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
